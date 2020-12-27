6316938a8e6dd5e439ef0107bd967a926f9aadf6
Su-Ching-Hsun <75729030+Su-Ching-Hsun@users.noreply.github.com>
2020-12-27 20:04:21 +0800

    Update ballgame.py

--- ballgame.py
+++ ballgame.py
@@ -1,32 +1,36 @@
 from campy.gui.events.timer import pause
-from breakoutgraphics2 import BreakoutGraphics
+from breakoutgraphics2 import BreakoutGraphics, Start
+import keyboard
 
 FRAME_RATE = 1000 / 120   # 120 frames per second.
-NUM_LIVES = 10
+NUM_LIVES = 5
 
 
 def main():
-    graphics = BreakoutGraphics()
-    lives = NUM_LIVES
-    while True:
-        if lives <= 0:
-            break
-        else:
-            pause(FRAME_RATE)
-            graphics.reflect()
-            graphics.move()
-            graphics.remove_and_score()
-            graphics.countlives()
-            if graphics.ball.y >= graphics.window.height:
-                lives -= 1
-                graphics.reset_ball()
-                graphics.switch_off()
-
+    start = Start()
+    if keyboard.read_key() == "space":
+        graphics = BreakoutGraphics()
+        start.window.close()
+        lives = NUM_LIVES
+        while True:
             if lives <= 0:
                 break
-            finished = graphics.finished()
-            if finished:
-                break
+            else:
+                pause(FRAME_RATE)
+                graphics.reflect()
+                graphics.move()
+                graphics.remove_and_score()
+                graphics.countlives()
+                if graphics.ball.y >= graphics.window.height:
+                    lives -= 1
+                    graphics.reset_ball()
+                    graphics.switch_off()
+
+                if lives <= 0:
+                    break
+                finished = graphics.finished()
+                if finished:
+                    break
 
 
 if __name__ == '__main__':
